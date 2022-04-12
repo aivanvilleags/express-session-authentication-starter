488ead04db337353a3c14cd4a68f99b2187189ea
aivan <haivan.villegas@gmail.com>
2022-04-12 13:24:15 -0500

    connected mongo db and added passport local strategy middleware

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,25 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const connection = require('./database');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const connection = require("./database");
 const User = connection.models.User;
 
-// TODO: passport.use();
+const customFIelds = {
+  usernameField: "uname",
+  passwordField: "pw",
+};
+
+const verifyCallback = (username, password, callback) => {
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        return callback(null, false);
+      }
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
+
+const strategy = new LocalStrategy();
+
+passport.use();
